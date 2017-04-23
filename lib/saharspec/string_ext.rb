606716f67515b053da6d67810e6e6dacf087460f
zverok <zverok.offline@gmail.com>
2017-04-23 18:36:02 +0300

    Fixup

--- lib/saharspec/string_ext.rb
+++ lib/saharspec/string_ext.rb
@@ -9,6 +9,6 @@ class String
   def unindent
     gsub(/\n\s+?\|/, "\n")    # for all lines looking like "<spaces>|" -- remove this.
     .gsub(/\|\n/, "\n")       # allow to write trailing space not removed by editor
-    .gsub(/\A\n|\n\s+\Z/, '') # remove empty strings before and after
+    .gsub(/\A\n|\n +\Z/, '')  # remove one empty string before and after, allows prettier %Q{}
   end
 end
