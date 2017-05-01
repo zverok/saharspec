83a4bcb56d9012fd2ec6ed5e9bf6e43277525232
zverok <zverok.offline@gmail.com>
2017-05-01 15:32:39 +0300

    More powerful its_map

--- lib/saharspec/its_map.rb
+++ lib/saharspec/its_map.rb
@@ -4,9 +4,17 @@ module RSpec
       # rubocop:disable Lint/NestedMethodDefinition
       describe("map(&:#{attribute})") do
         let(:__its_map_subject) do
-          attribute_chain = attribute.to_s.split('.').map(&:to_sym)
-          attribute_chain.inject(subject) do |inner_subject, attr|
-            inner_subject.map(&attr)
+          if Array === attribute
+            if Hash === subject
+              attribute.inject(subject) {|inner, attr| inner.map { |h| h[attr] } }
+            else
+              subject.map { |inner| inner[*attribute] }
+            end
+          else
+            attribute_chain = attribute.to_s.split('.').map(&:to_sym)
+            attribute_chain.inject(subject) do |inner_subject, attr|
+              inner_subject.map(&attr)
+            end
           end
         end
 
