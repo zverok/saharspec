98594a1fab34c44a5f5188aef7a6e2c165e6dd45
zverok <zverok.offline@gmail.com>
2017-08-12 21:39:50 +0300

    Rubocop

--- spec/saharspec/its/call_spec.rb
+++ spec/saharspec/its/call_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe :its_call do
   subject { relevant << 'foo' }
 
   its_call { is_expected.to change { relevant }.to(['foo']) }
-  its_call { is_expected.not_to change { irrelevant } }
+  its_call { is_expected.not_to(change { irrelevant }) }
 
   describe 'metadata passing' do
     before(:each, :some_metadata) { relevant << 'bar' }
 
     its_call { is_expected.to change { relevant }.to(['foo']) }
-    its_call(:some_metadata) { is_expected.to change { relevant }.to(['bar', 'foo']) }
+    its_call(:some_metadata) { is_expected.to change { relevant }.to(%w[bar foo]) }
   end
 end
