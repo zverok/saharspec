74768a9e168b3fb9ad7a20dcc85ab9398671d212
zverok <zverok.offline@gmail.com>
2023-11-18 18:38:38 +0200

    Make lets: metadata trick work

--- spec/saharspec/metadata/lets_spec.rb
+++ spec/saharspec/metadata/lets_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe :metadata_lets do
 
   context 'when lets is set', lets: {foo: 'bar'} do
     it { expect(foo).to eq 'bar' }
+
+    context 'when lets are nesting', lets: {foo: 'baz', baz: 'test'} do
+      it { expect(baz).to eq 'test' }
+      it { expect(foo).to eq 'baz' }
+    end
   end
 
   context 'when lets is adjusting the other let', lets: {y: 5} do
@@ -12,6 +17,27 @@ RSpec.describe :metadata_lets do
   end
 
   context '', lets: {x: 100, title: "OK"} do
-    it { expect(self.class.description).to eq 'with x=100, title="OK"' }
+    it { |ex| expect(ex.full_description).to eq 'metadata_lets x=100 title="OK" ' }
+    it { expect(self.class.description).to eq 'x=100 title="OK"' }
+
+    context '', lets: {y: 200} do
+      it { |ex| expect(ex.full_description).to eq 'metadata_lets x=100 title="OK" y=200 ' }
+      it { expect(self.class.description).to eq 'y=200' }
+    end
+
+    context '', lets: {x: 500} do
+      it { |ex| expect(ex.full_description).to eq 'metadata_lets x=100 title="OK" x=500 ' }
+      it { expect(self.class.description).to eq 'x=500' }
+    end
+  end
+
+  context 'with ', lets: {x: 100, title: "OK"} do
+    it { |ex| expect(ex.full_description).to eq 'metadata_lets with x=100 title="OK" ' }
+    it { expect(self.class.description).to eq 'with x=100 title="OK"' }
+
+    context 'when ', lets: {y: 200} do
+      it { |ex| expect(ex.full_description).to eq 'metadata_lets with x=100 title="OK" when y=200 ' }
+      it { expect(self.class.description).to eq 'when y=200' }
+    end
   end
 end
